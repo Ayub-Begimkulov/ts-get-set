4088a9ce2219c6749712ee06b1c4f79cde482000
Ayub-Begimkulov <begimkulov963@gmail.com>
2021-04-25 06:33:47 +0300

    removed unnecessary readonly

--- src/string-to-path.ts
+++ src/string-to-path.ts
@@ -2,15 +2,14 @@ export type StringToPath<StringPath extends string> = StringToPath_<StringPath>;
 
 type StringToPath_<
   StringPath extends string,
-  Path extends readonly string[] = []
+  Path extends string[] = []
 > = StringPath extends `${infer Key}.${infer Rest}`
   ? StringToPath_<Rest, AppendPath<Path, Key>>
   : AppendPath<Path, StringPath>;
 
-type AppendPath<
-  Path extends readonly string[],
-  Item extends string
-> = Item extends "" ? Path : [...Path, Item];
+type AppendPath<Path extends string[], Item extends string> = Item extends ""
+  ? Path
+  : [...Path, Item];
 
 export const stringToPath = <T extends string>(path: T) =>
   path.split(".").filter(Boolean) as StringToPath<T>;
